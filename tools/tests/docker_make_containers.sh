0e00c0b9688cad11ea2ba96be9a977dd963fb46e
yan zhai <zhaiyan920@gmail.com>
2017-02-26 22:47:27 -0600

    fix a minor bug

--- tools/tests/docker_make_containers.sh
+++ tools/tests/docker_make_containers.sh
@@ -2,7 +2,8 @@
 
 target=${1:-dead}
 id=${2:-`uuidgen | tr -d -`}
-tooldir=`readlink -f .`
+toolexec=`readlink -f $0`
+tooldir=`dirname $toolexec`
 srcdir=$tooldir/../../tests/$target
 containerdir=$tooldir/../../tests/containers/$id
 
