3d8ec5eacb8a03d9a7a8ee0956f7a46d1f36b2b4
Yan Zhai <yanzhai@cs.wisc.edu>
2017-03-05 23:02:11 -0600

    add rm container script

---
+++ tools/tests/rm_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  return 0
+fi
+
+toolexec=`readlink -f $0`
+tooldir=`dirname $toolexec`
+for id in $#; do
+  if [[ x"$id" == x ]]; then
+    continue
+  fi 
+  rm -r $tooldir/../../tests/containers/$id
+done
+
